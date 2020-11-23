6c637be1114cfdafdc023a65b8f1a63687bd5cb8
unbegrenzt <jmoralescenteno@gmail.com>
2020-11-23 11:10:35 -0600

    cambiando estilo del mapa

--- src/components/PickerMapView/PickerMapView.jsx
+++ src/components/PickerMapView/PickerMapView.jsx
@@ -46,8 +46,8 @@ const PickerMapView = () => {
                 style={{ height: `${window.innerHeight - navHeight}px` }}
             >
                 <TileLayer
-                    attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-                    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <PickMarker startPos={startPos}
                     eventHandlers={eventHandlers}
